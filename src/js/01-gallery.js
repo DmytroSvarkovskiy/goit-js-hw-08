dff1fd37bd02e48896970e82290100dd7a002be2
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-23 21:30:44 +0300

    Добавив 1 дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,20 @@
 
-// import cardTemp from "../template/cards.hbs"
+
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+// import cardTemp from "../template/cards.hbs"
 
 
 const galaryDiv = document.querySelector('.gallery');
+const createGalery = collection => {
+    collection.map(({ preview, original, description }) => {
+        const markingElement = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+        galaryDiv.insertAdjacentHTML('afterbegin', markingElement);
+    })
+}
 
 // const createGalery = collection => 
 //     galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
