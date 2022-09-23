147ca75218543f1f9947152efefa833a463d17bc
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-23 21:48:55 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,21 +3,12 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// import cardTemp from "../template/cards.hbs"
-
+import cardTemp from "../template/cards.hbs"
 
 const galaryDiv = document.querySelector('.gallery');
-const createGalery = collection => {
-    collection.map(({ preview, original, description }) => {
-        const markingElement = `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}"/>
-</a>`;
-        galaryDiv.insertAdjacentHTML('afterbegin', markingElement);
-    })
-}
 
-// const createGalery = collection => 
-//     galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
+const createGalery = collection => 
+    galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
     
 createGalery(galleryItems)
 
