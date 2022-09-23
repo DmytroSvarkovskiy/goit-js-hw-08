d7124472b3fe5a694a296701badc39d3404270f8
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-09-23 20:55:10 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import cardTemp from "../template/cards.hbs"
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection => 
+    galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
+    
+createGalery(galleryItems)
+
+
+const onImgClick = event => {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+}
+galaryDiv.addEventListener('click', onImgClick);
